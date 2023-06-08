5d18ba35b2e290f194ccd785819e5a566fc72a34
Lars Schöbitz <lschoebitz@ethz.ch>
2023-06-08 15:45:28 +0200

    Add and tidy data

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,3 +1,9 @@
+
+
+library(readr)
+
+kitas <- read_csv("data-raw/kennzahlen_kitas_seit2006_klein.csv")
+
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(DATASET, overwrite = TRUE)
